26e3740e8bce6b0e56d505c4a9a4c05462a9f9ac
Miguel Carvalho <74492413+MiguelCarvalho007@users.noreply.github.com>
2025-08-16 20:30:18 -0300

    Update generate_lang_svg.py

--- generate_lang_svg.py
+++ generate_lang_svg.py
@@ -1,12 +1,14 @@
 import json
 import matplotlib.pyplot as plt
 
+# Abre o JSON gerado pelo workflow
 with open("languages.json") as f:
     data = json.load(f)
 
 # Ordena por mais usado e pega top 5
 data = dict(sorted(data.items(), key=lambda x: x[1], reverse=True)[:5])
 
+# Gera o gráfico
 plt.figure(figsize=(5,2))
 plt.bar(data.keys(), data.values(), color="#6272a4")
 plt.title("Most Used Languages")
